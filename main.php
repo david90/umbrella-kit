cf0310a7be05951db108aa8f50c8f1336eed1679
david90 <david.ng@oursky.com>
2014-10-06 21:28:46 +0800

    Update website structure

--- main.php
+++ main.php
@@ -13,9 +13,10 @@
   <link rel="import" href="/components/paper-tabs/paper-tabs.html">
   <link rel="import" href="/components/paper-input/paper-input.html">
   <link rel="import" href="/components/paper-button/paper-button.html">
-  <link rel="import" href="post-list.html">
-  <link rel="import" href="page-about.html">
-  <link rel="import" href="page-credits.html">
+  <link rel="import" href="/post-list.html">
+  <link rel="import" href="/page-about.html">
+  <link rel="import" href="/page-credits.html">
+  <link rel="import" href="/page-embed-imgur.html">
   <link rel="stylesheet" type="text/css" href="/stylesheets/main.css">
   <!--plugin-->
   <link type="text/css" rel="stylesheet" href="stylesheets/lightGallery.css" />
@@ -30,18 +31,32 @@
       <paper-tabs selected="about" valueattr="name" self-end>
         <paper-tab name="about">About</paper-tab>
         <paper-tab name="all">Kits</paper-tab>
-        <paper-tab name="pdf">PDFs</paper-tab>
+        <paper-tab name="summary">Summary</paper-tab>
         <paper-tab name="credits">Credits</paper-tab>
       </paper-tabs>
     </core-toolbar>
     <div class="container" layout vertical center>
-      <!-- <paper-input label="Search"></paper-input> -->
       <post-list show="all"></post-list>
       <page-about hide="true"></page-about>
       <page-credits hide="true"></page-credits>
+      <page-embed-imgur hide="true"></page-embed-imgur>
       <paper-button raised id="dynamic" class="info-btn" label="Info Kit"></paper-button>
     
+  <!--Disqus-->
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        var disqus_shortname = 'umbrellastory';
 
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+ 
     </div>
   </core-header-panel>
 
@@ -51,25 +66,31 @@
 
     var page_about = document.querySelector('page-about');
     var page_credits = document.querySelector('page-credits');
+    var page_embed_imgur = document.querySelector('page-embed-imgur');
 
     tabs.addEventListener('core-select', function() {
-      
-
+      history.pushState(null,null,'#'+tabs.selected);
       if (tabs.selected == "all" || tabs.selected == "pdf") {
         list.show = tabs.selected;
         page_credits.hide = "true";
+        page_embed_imgur.hide = "true";
         page_about.hide = "true";
       }
       else if (tabs.selected == "about") {
-        //page_about = 
         page_about.hide = "false";
         page_credits.hide = "true";
+        page_embed_imgur.hide = "true";
         list.show = "none";
       }
       else if (tabs.selected == "credits") {
-        //page_about = 
         page_about.hide = "true";
         page_credits.hide = "false";
+        page_embed_imgur.hide = "true";
+        list.show = "none";
+      } else if (tabs.selected == "summary") {
+        page_about.hide = "true";
+        page_credits.hide = "true";
+        page_embed_imgur.hide = "false";
         list.show = "none";
       }
     });
@@ -79,12 +100,16 @@
   <!--lightGallery-->
   <script type="text/javascript">
   $(document).ready(function() {
-    $('#dynamic').click(function(e){
-        $(this).lightGallery({
-            dynamic:true,
-            html:true,
-            mobileSrc:true,
-            dynamicEl:[
+    // if have hash
+    console.log("window.location.hash="+window.location.hash);
+    var hash = window.location.hash;
+    if (hash) {
+      var hashName = hash.replace("#", "");
+      var tabs = document.querySelector('paper-tabs');
+      tabs.select = hashName;
+    }
+
+    var imagList = [
                 {"src":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/1.png","thumb":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/1.png", "mobileSrc":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/1.png"},
                 {"src":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/2.png","thumb":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/2.png", "mobileSrc":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/2.png"},
                 {"src":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/3.png","thumb":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/3.png", "mobileSrc":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/3.png"},
@@ -113,27 +138,20 @@
                 {"src":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/26.png","thumb":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/26.png", "mobileSrc":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/26.png"},
                 {"src":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/27.png","thumb":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/27.png", "mobileSrc":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/27.png"},
                 {"src":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/28.png","thumb":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/28.png", "mobileSrc":"https://dfg63nb4d89j7.cloudfront.net/zh-hk/28.png"}
-            ]
+            ];
+
+
+    $('#dynamic').click(function(e){
+        $(this).lightGallery({
+            dynamic:true,
+            html:true,
+            mobileSrc:true,
+            dynamicEl: imageList;
         }); 
     }) 
   });
 </script>
 
-  <!--Disqus-->
- <!--    <div id="disqus_thread"></div>
-    <script type="text/javascript">
-        var disqus_shortname = 'umbrellastory';
-
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
- -->
   <!--Analytics-->
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
