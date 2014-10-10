0c188e4fb626fed3b422ff6b7232d070ee988b15
david90 <david.ng@oursky.com>
2014-10-11 00:44:42 +0800

    Fix spelling mistake on english og:image

--- meta/en_US_meta.php
+++ meta/en_US_meta.php
@@ -6,7 +6,7 @@
 <!-- <meta property="og:image" content="https://dfg63nb4d89j7.cloudfront.net/web/meta/og-thumb_en_US.png" />    
 <meta property="og:image:url" content="https://dfg63nb4d89j7.cloudfront.net/web/meta/og-thumb_en_US.png" />
  -->
-<meta property="og:image" content="<? echo $protocol . $_SERVER['HTTP_HOST']?>/img/og-thumb-3-en.png" />
+<meta property="og:image" content="<? echo $protocol . $_SERVER['HTTP_HOST']?>/img/og-thumb-3-en.png?20141011" />
  <meta property="og:image:type" content="image/png" />
 <!-- <meta property="og:image:width" content="1200" />
 <meta property="og:image:height" content="630" /> -->
